953a4859b2780196467ab35edb3235e61378cb5e
Callum Alexander <alexandercallum4@gmail.com>
2024-10-03 10:41:24 +0100

    Created second solution

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
